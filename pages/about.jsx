efb10f8bd08f00f813d56d738e6e4a9fd9193e08
Jesus <jisena32@gmail.com>
2022-06-27 18:49:12 -0400

    mi-primer-proyecto

---
+++ pages/about.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout"
+
+export default function about() {
+  return (
+    <Layout
+      title="About | next.js"
+      description="Pagina Principal"
+    >
+      <h1>About</h1>
+    </Layout>
+  )
+}
